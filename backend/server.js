a4753303506f08afab34a64be0c008754b84206c
Srividya <srividyakambhampati92@gmail.com>
2026-02-18 15:52:34 +0530

    updated global port

--- backend/server.js
+++ backend/server.js
@@ -91,6 +91,6 @@ mongoose.connect(mongoURI)
 ====================== */
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-   console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+   console.log(`🚀 Server running on port ${PORT}`);
 });
